61e547aec20ba151a7ea4e138a0e250ca50dde10
UserLaptop <adam.bakhmadov@student.ap.be>
2024-11-17 23:31:24 +0100

    delete comments

--- myGame/myGame/Hero.cs
+++ myGame/myGame/Hero.cs
@@ -20,12 +20,8 @@ namespace myGame
         public Hero(Texture2D texture)
         {
             heroTexture = texture;
-            //deelRectangle = new Rectangle(moveOn_x, 0, 190, 225);
-            //512x256
-           //7
 
             animatie = new Animatie();
-           // animatie.AddFrame(new AnimationFrame(new Rectangle(0, 0, 73, 64)));
             animatie.AddFrame(new AnimationFrame(new Rectangle(1, 1, 68, 56)));
             animatie.AddFrame(new AnimationFrame(new Rectangle(70, 1, 68, 56)));
             animatie.AddFrame(new AnimationFrame(new Rectangle(1, 1, 68, 56)));
